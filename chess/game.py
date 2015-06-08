664366a4c63d6e7eba2357560da24238fb5ffd81
Fred Frey <f.frey6@gmail.com>
2015-06-08 02:57:55 -0400

    Finished game.py for basic functionality

---
+++ chess/game.py
@@ -0,0 +1,32 @@
+from chess.piece import Piece
+from chess.utils import *
+
+class Game:
+	current_board = [[Piece("Null", "Null", "Null") for x in range(8)] for x in range(8)]
+	def __init__(self):
+		self.current_board[0][0] = Piece("rook", "white", "a1")
+		self.current_board[1][0] = Piece("knight", "white", "b1")
+		self.current_board[2][0] = Piece("bishop", "white", "c1")
+		self.current_board[3][0] = Piece("queen", "white", "d1")
+		self.current_board[4][0] = Piece("king", "white", "e1")
+		self.current_board[5][0] = Piece("bishop", "white", "f1")
+		self.current_board[6][0] = Piece("knight", "white", "g1")
+		self.current_board[7][0] = Piece("rook", "white", "h1")
+		
+		self.current_board[0][7] = Piece("rook", "black", "a8")
+		self.current_board[1][7] = Piece("knight", "black", "b8")
+		self.current_board[2][7] = Piece("bishop", "black", "c8")
+		self.current_board[3][7] = Piece("queen", "black", "d8")
+		self.current_board[4][7] = Piece("king", "black", "e8")
+		self.current_board[5][7] = Piece("bishop", "black", "f8")
+		self.current_board[6][7] = Piece("knight", "black", "g8")
+		self.current_board[7][7] = Piece("rook", "black", "h8")
+		
+		for i in range(0,8):
+			self.current_board[i][1] = Piece("pawn", "white", cartesian_to_algebraic([i, 1]))
+			self.current_board[i][6] = Piece("pawn", "black", cartesian_to_algebraic([i, 6]))
+	
+	def piece_at(self, position):
+		target = algebraic_to_cartesian(position)
+		print(target)
+		return self.current_board[target[0]][target[1]]
--- chess/piece.py
+++ chess/piece.py
@@ -1,11 +1,14 @@
 '''
-Piece class for Computer Science chess final project
+Piece module for Computer Science chess final project
 
 Contains all methods related to piece movement 
 '''
+from chess.utils import *
 
 class Piece:
-	piece_type, color, position = "Null"
+	piece_type = "Null"
+	color = "Null"
+	position = "Null"
 	
 	def __init__(self, piece_type, color, position): 
 		self.piece_type = piece_type
@@ -15,22 +18,35 @@ class Piece:
 			color refers to which player the piece belongs (black or white)
 			position describes the location of the piece on the board (e.g. a5)'''
 		
-	def get_moves(self):
-		'''
+	def get_moves(self, board):
 		if self.piece_type == "pawn":
-			
+			return self.get_pawn_moves(board)
 		elif self.piece_type == "rook":
-			#TODO: ...
+			return self.get_rook_moves(board)
 		elif self.piece_type == "knight":
-			#TODO: ...
+			return self.get_knight_moves(board)
 		elif self.piece_type == "bishop":
-			#TODO: ...
+			return self.get_bishop_moves(board)
 		elif self.piece_type == "queen":
-			#TODO: ...
+			return self.get_queen_moves(board)
 		elif self.piece_type == "king":
-			#TODO: ...
+			return self.get_king_moves(board)
+		
 		else:
 			return "Error: No piece type defined for object #" + id(self)
-			'''
+	
+	def get_pawn_moves(self, board):
+		moves = []
+		coords = algebraic_to_cartesian(self.position)
+		av1 = cartesian_to_algebraic([coords[0] - 1, coords[1] + 1])
+		av2 = cartesian_to_algebraic([coords[0] + 1, coords[1] + 1])
+		av3 = cartesian_to_algebraic([coords[0], coords[1] + 1])
 		
-		print("TODO: ...")
+		if board.piece_at(av3).piece_type == "Null":
+			moves.append(self.position +  av3)
+		if board.piece_at(av1).color != "Null" and board.piece_at(av1).color != self.color:
+			moves.append(self.position + av1)
+		if board.piece_at(av2).color != "Null" and board.piece_at(av2).color != self.color:
+			moves.append(self.position + av2)
+		
+		return moves
