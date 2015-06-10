afacbbf06ce4a8b885b60b65159300a82ccb927c
Fred Frey <f.frey6@gmail.com>
2015-06-10 04:25:00 -0400

    started work on castling.

--- chess/game.py
+++ chess/game.py
@@ -3,6 +3,8 @@ from chess.piece import *
 
 class Game:
 	current_board = None
+	castle_white = None
+	castle_black = None
 	def __init__(self):
 		self.current_board = [[Piece("Null", "Null", "Null") for x in range(8)] for x in range(8)]
 		self.current_board[0][0] = Piece("rook", "white", "a1")
@@ -27,22 +29,71 @@ class Game:
 			self.current_board[i][1] = Piece("pawn", "white", cartesian_to_algebraic([i, 1]))
 			self.current_board[i][6] = Piece("pawn", "black", cartesian_to_algebraic([i, 6]))
 		
+		self.castle_white = [True, True] # index 0 is king's side castling, index 1 is queen's
+		self.castle_black = [True, True]
+		
 	def piece_at(self, position):
 		target = algebraic_to_cartesian(position)
 		return self.current_board[target[0]][target[1]]
 	
 	def move_piece(self, action):
-		try:
-			start_square = algebraic_to_cartesian(action[:2])
-			end_square = algebraic_to_cartesian(action[2:])
+		if action == "e1g1" and self.castle_white[0]:
+			self.current_board[6][0] = Piece("king", "white", "g1")
+			self.current_board[5][0] = Piece("rook", "white", "f1")
+			self.current_board[4][0] = Piece("Null", "Null", "Null")
+			self.current_board[7][0] = Piece("Null", "Null", "Null")
 			
-			self.current_board[end_square[0]][end_square[1]] = self.piece_at(action[:2])
-			self.current_board[start_square[0]][start_square[1]] = Piece("Null", "Null", "Null")
-			
-			self.current_board[end_square[0]][end_square[1]].position = action[2:]
 		
-		except Exception as e:
-			print("\n" + str(e) + "\n")
+		elif action == "e1c1" and self.castle_white[1]:
+			self.current_board[2][0] = Piece("king", "white", "c1")
+			self.current_board[3][0] = Piece("rook", "white", "d1")
+			self.current_board[4][0] = Piece("Null", "Null", "Null")
+			self.current_board[0][0] = Piece("Null", "Null", "Null")
+			
+			
+		elif action == "e8g8" and self.castle_white[0]:
+			self.current_board[6][7] = Piece("king", "black", "g8")
+			self.current_board[5][7] = Piece("rook", "black", "f8")
+			self.current_board[4][7] = Piece("Null", "Null", "Null")
+			self.current_board[7][7] = Piece("Null", "Null", "Null")
+			
+		
+		elif action == "e8c8" and self.castle_white[1]:
+			self.current_board[2][7] = Piece("king", "black", "c8")
+			self.current_board[3][7] = Piece("rook", "black", "d8")
+			self.current_board[4][7] = Piece("Null", "Null", "Null")
+			self.current_board[0][7] = Piece("Null", "Null", "Null")
+			
+		if action[:2] == "e1":
+			self.castle_white[0] = False
+			self.castle_white[1] = False
+		if action[:2] == "h1":
+			self.castle_white[0] = False
+		if action[:2] == "a1":
+			self.castle_white[1] = False
+		
+		if action[:2] == "e8":
+			self.castle_black[0] = False
+			self.castle_black[1] = False
+		if action[:2] == "h8":
+			self.castle_black[0] = False
+		if action[:2] == "a8":
+			self.castle_black[1] = False
+			
+		else:	
+			try:
+				start_square = algebraic_to_cartesian(action[:2])
+				end_square = algebraic_to_cartesian(action[2:])
+				
+				self.current_board[end_square[0]][end_square[1]] = self.piece_at(action[:2])
+				self.current_board[start_square[0]][start_square[1]] = Piece("Null", "Null", "Null")
+				
+				self.current_board[end_square[0]][end_square[1]].position = action[2:]
+			
+			except Exception as e:
+				print("\n" + str(e) + "\n")
+		
+		
 	
 	def illegal_moves(self, moves_list, color):
 		moves = []
@@ -76,5 +127,17 @@ class Game:
 		
 		rejected_moves = self.illegal_moves(moves, color)
 		moves = [x for x in moves if x not in rejected_moves]
+		
+		if color == "white":
+			if self.castle_white[0] and self.piece_at("f1").piece_type == "Null" and self.piece_at("g1") == "Null":
+				moves.append("e1g1")
+			if self.castle_white[1] and self.piece_at("b1").piece_type == "Null" and self.piece_at("c1") == "Null" and self.piece_at("d1") == "Null":
+				moves.append("e1c1")
+		
+		if color == "black":
+			if self.castle_black[0] and self.piece_at("f8").piece_type == "Null" and self.piece_at("g8") == "Null":
+				moves.append("e8g8")
+			if self.castle_black[1] and self.piece-at("b8").piece_type == "Null" and self.piece_at("c8") == "Null" and self.piece_at("d8") == Null:
+				moves.append("e8c8")
 		
 		return moves
