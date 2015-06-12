afc6df8d62934c67c64d8316fdf0fb959d7da56a
blueForestIcarus <bficarus@localhost.localdomain>
2015-06-12 05:11:23 +0000

    gui

---
+++ chess/gui.py
@@ -0,0 +1,267 @@
+
+
+import time, os, curses, shutil, sys, random
+import math
+from termcolor import cprint
+from multiprocessing import Process
+import termbox
+from optparse import OptionParser
+import rpyc
+
+class FTF:
+    current = None
+    frames = None
+
+    path = None
+    out = None
+
+    name = None
+    frameCount = None
+    frameHeight = None
+    frameWidth = None
+    delay = None
+    
+
+    def __init__(self, path, out):
+        self.path = path
+        self.out = out
+
+        doc = open(path, "r")
+        file = doc.read()
+        doc.close()
+
+        lines = file.split("\n")[:]
+        meta = lines.pop(0).split(":")
+
+        self.name = meta[0].split("--")[0]
+        self.frameCount = int(meta[1])
+        self.frameHeight = int(meta[2])
+        self.frameWidth = int(meta[3])
+        self.delay = float(meta[4])
+
+        self.current = 0
+        self.frames = []
+        for i in range(self.frameCount):
+            frame = []
+            for l in range(self.frameHeight):
+                line = lines.pop(0)
+                while len(line) < self.frameWidth:
+                    line += " "
+
+                while len(line) > self.frameWidth:
+                    line = line[:-1]
+            
+                frame.append(line)
+            self.frames.append(frame)
+
+    def switch(self, index):
+        self.current = index
+        self.out(self.frames[index])
+
+    def animateYield(self,repeat=-1):
+        while repeat is not 0:
+            last = time.time()
+            while time.time() - last < self.delay:
+                yield
+
+            self.switch((self.current + 1)%self.frameCount)
+           
+            if repeat is not -1:
+                repeat -= 1;
+
+    def animateProcess(self):
+        pass
+
+    def animateDelay():
+        pass
+
+
+class Graphic:
+    posx = 0
+    posy = 0
+    maxx = 0
+    maxy = 0
+
+    window = None
+    path = None
+    ftf = None
+
+    autoRefresh_flg = True
+
+    def __init__(self, path, window, posx=0, posy=0, maxx=0, maxy=0):
+        self.posx = posx
+        self.posy = posy
+        self.maxx = maxx
+        self.maxy = maxy
+        self.window = window
+        self.path = path
+
+        self.ftf = FTF(path, out=self.print_frame)
+
+    def resizeToFTF(self,x,y):
+        if x > 0 :
+            self.posx = x - 1
+            self.maxx = self.posx + self.ftf.frameWidth
+        elif x < 0:
+            self.maxx = self.window.width() + x + 1
+            self.posx = self.maxx - self.ftf.frameWidth
+        else:
+            self.posx = math.floor(self.window.width()/2 - self.ftf.frameWidth/2)
+            self.maxx = self.posx + self.ftf.frameWidth
+
+        if y > 0 :
+            self.posy = y - 1
+            self.maxy = self.posy + self.ftf.frameHeight
+        elif y < 0:
+            self.maxy = self.window.height() + y + 1
+            self.posy = self.maxy - self.ftf.frameHeight
+        else:
+            self.posy = math.floor(self.window.height()/2 - self.ftf.frameHeight/2)
+            self.maxy = self.posy + self.ftf.frameHeight
+    def moveToLimit(self, x ,y):
+        if(x>0): 
+            if(self.posx<x):
+                delta = x-self.posx
+                self.posx+=delta
+                self.maxx+=delta
+        if(y>0):
+            if(self.posy<y):
+                delta = y-self.posy
+                self.posy+=delta
+                self.maxy+=delta
+        if(x<0): 
+            x *= -1
+            if(self.maxx>x):
+                delta = x-self.posx
+                self.posx+=delta
+                self.maxx+=delta
+        if(y<0):
+            y *= -1
+            if(self.maxy>y):
+                delta = self.maxy-y
+                self.posy-=delta
+                self.maxy-=delta
+        
+    def print_char(self, x, y, char, fg, bg, update=None):
+        gx = x+self.posx
+        gy = y+self.posy
+        
+        self.window.change_cell(gx, gy, ord(char), fg, bg)
+
+        if (update is None and self.autoRefresh_flg) or update:
+            self.flush()
+    
+    def print_frame(self, frame, fg , bg,  update=None):
+        for y in range(len(frame)):
+            line = frame[y]
+            for x in range(len(line)):
+                char = line[x]
+                self.print_char(x, y, char,fg, bg, update=False )
+        
+        if (update is None and self.autoRefresh_flg) or update:
+            self.flush()
+
+    def print_string(self, x, y, line, update=None):
+        for i in range(len(line)):
+            char = line[i]
+            self.print_char(x+i, y, char, termbox.DEFAULT, termbox.DEFAULT, update=False )
+
+        if (update is None and self.autoRefresh_flg) or update:
+            self.flush()
+
+    def flush(self):
+        self.window.present()
+
+    def set_state(self, state):
+        self.ftf.switch(state)
+
+    def set_state_adv(self, state, fg , bg):
+        self.print_frame(self.ftf.frames[state], fg , bg, update=False)
+
+    def clear(self, char=" "):
+        for x in range(self.posx, self.maxx):
+            for y in range(self.posy, self.maxy):
+                self.print_char(x, y, char, termbox.DEFAULT, termbox.DEFAULT, update=False )
+        
+        self.flush()
+
+box = None    
+display = None
+
+def initGui():
+    global box, display
+    box = termbox.Termbox()
+    box.clear()
+    box.present()
+    display = []
+    for x in range(8):
+        column = []
+        for y in range(8):
+            g = Graphic("piece.txt", box)
+            g.posx = x * 8
+            g.posy = y * 8
+            g.maxx = g.posx + 8
+            g.maxy = g.posy + 8
+
+            if random.randrange(1,3) == 1:
+                fg = termbox.BLACK
+                bg = termbox.WHITE
+            else:
+                fg = termbox.WHITE
+                bg = termbox.BLACK
+
+            g.set_state_adv(random.randrange(0,7),fg,bg)
+            g.flush()
+            column.append(g)
+        display.append(column)
+
+def closeGui():
+    global box
+    box.close()
+
+def updateGuiList(board):
+    global display
+    for x in range(len(board)):
+        for y in range(len(board)):
+            piece = board[x][y]
+
+            index = get_state(piece)
+            if piece.color == "black":
+                fg = termbox.BLACK
+                bg = termbox.WHITE
+            else:
+                fg = termbox.WHITE
+                bg = termbox.BLACK
+
+            display[x][y].set_state_adv(index, fg , bg)
+            display.flush()
+
+def get_state(self, piece):
+    if piece == "pawn":
+        return 1
+    elif piece == "rook":
+        return 2
+    elif piece == "knight":
+        return 3
+    elif piece == "bishop":
+        return 5
+    elif piece == "queen":
+        return 4
+    elif piece == "king":
+        return 6
+    else:
+        return 0
+
+try:
+    initGui()
+except:
+    if box:
+        box.close()
+
+    raise
+
+for i in range(5):
+   time.sleep(1)
+
+
+box.close()
